0b7c3939f0816c1e29714b7165804d50379ce52d
jaswinder135 <61361101+jaswinder135@users.noreply.github.com>
2021-02-22 23:24:31 -0500

    commit

---
+++ backend/data/products.js
@@ -0,0 +1,112 @@
+const products = [
+  {
+    name: 'Airpods Wireless Bluetooth Headphones',
+    image: '/images/airpods.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Brady Upholsterd Queen Bed',
+    image: 'https://cdn.shopify.com/s/files/1/2660/5202/products/xoir1j2ihw9b4bw2apj5_a6ba22e9-258d-435e-86c6-7fadbe115a84_1400x.jpg?v=1598896506',
+    description:
+      'Blending traditional style with modern materials, the Brady upholstered bed brings comfortable elegance to your bedroom.',
+    brand: 'Brady',
+    category: 'Bedding',
+    price: 589.99,
+    countInStock: 6,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Full Sleeve T-shirt',
+    image: 'https://5.imimg.com/data5/NX/UD/IG/ANDROID-108846742/ms-1-512-5715383-2faab753-l-jpg-500x500.jpg',
+    description:
+      'Shirts Men Cotton Linen Designer Brand Loose Fit Man Shirts Long Sleeve Pullover Shirts For Men Clothes Summer Wear',
+    brand: 'Polo',
+    category: 'Clothe',
+    price: 99.99,
+    countInStock: 9,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Trousers',
+    image: 'https://sc04.alicdn.com/kf/UTB89K8FwgnJXKJkSael760UzXXa8.png',
+    description:
+      'Defrost post-match with the Flames fitted track. A closed ankle cuff,',
+    brand: 'Polo',
+    category: 'Clothes',
+    price: 79.99,
+    countInStock: 9,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Playstation 4 Pro White Version',
+    image: '/images/playstation.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: 'https://www.byrdie.com/thmb/_cq5pPLCWopDzQ2MbD6wfIKzqn4=/700x700/smart/filters:no_upscale()/cdn.cliqueinc.com__cache__posts__267804__how-to-fix-broken-make-up-267804-1536963220742-main.700x0c-b9a0902ed681466d8f1720f5758bdf55.jpg',
+    description:
+      'Regardless of your skin type, there is a primer for you, whether you’re looking for a formula to control oil and/or acne, moisturize, smooth out uneven texture, color correct, etc.',
+    brand: 'Sephora',
+    category: 'Makeup',
+    price: 59.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg',
+    description:
+      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
